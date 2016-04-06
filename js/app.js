d2b18ca06335f92b0234c36e05e9c2192274f434
Maudulus <Maude.jake@gmail.com>
2016-04-06 15:57:23 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$('.menu .button, .reveal-modal-bg').on('click',function(event) {
+	if( $(event.target).attr('class') == 'reveal-modal-bg' || $(event.target).attr('class') == 'small button') {
+		$('.reveal-modal-bg').fadeToggle();
+		$('.nav').toggleClass('open');		
+	}
+});
